bfebaf16117b6f36ba58b0e8a68d43e22baf44c3
Martin Dreier <1568928+mdreier@users.noreply.github.com>
2022-03-17 17:55:44 +0100

    Übung 1.2

---
+++ R22_1.2_Hilfe/R12_Hilfe_2.R
@@ -0,0 +1,29 @@
+codeoceanR::rt_score()
+
+# A6 ----
+# Wieviele Seiten hat das RefCard von Tom Short?
+refCardAnzahlSeiten <- 0
+
+
+# A7 ----
+# Im Rstudio base R cheat sheet steht die Syntax, um die Doku von Funktionen aufzurufen.
+# Welche Funktion wird dort als erstes Beispiel verwendet?
+cheatSheetFunktion <- "Funktionsname"
+
+
+# Pro Tipp: drucke eine RefCard deiner Wahl aus und markiere 
+# nach und nach farblich, was du alles gelernt und verstanden hast.
+
+
+# A8 ----
+# Was ist der Link zu aktuellen R Fragen auf Stackoverflow? (der https Teil ist optional)
+soLinkR <- "URL_hier"
+
+
+# A9 ----
+# Welchen Beitrag im Kursforum findest du bisher am hilfreichsten?
+forumLink <- "URL_hier"
+
+
+# Wenn du fertig bist, übermittle bitte deinen Punktestand an openHPI mit:
+# codeoceanR::rt_submit()
