f58f24039ccf7003fd10019fde2426778ab85460
Martin Dreier <1568928+mdreier@users.noreply.github.com>
2022-03-17 17:55:39 +0100

    Übung 0.4

---
+++ R22_0.4_Uebungsaufgaben/R04_Aufgaben_2.R
@@ -0,0 +1,18 @@
+codeoceanR::rt_score()
+# Siehe Anleitung im Kursvideo / PDF Abschnitt 1.4 Übungsaufgaben
+
+# A3 ----
+# Teile 294 durch 7.
+
+
+
+# A4 ----
+# Multipliziere 12 mit 3,5.
+# Tipp: Dezimalzahlen werden beim Programmieren nicht mit einem Komma dargestellt.
+
+
+
+# Wenn du fertig bist, übermittle bitte deinen Score an openHPI.
+# Im Browser: klicke nach einem Bewerten rechts auf "Code zur Bewertung abgeben"
+# In Rstudio: führe folgendes aus:
+# codeoceanR::rt_submit()
