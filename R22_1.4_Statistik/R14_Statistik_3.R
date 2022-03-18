baf2b7fa147de84722b244bf4636eef0ed81b20a
Martin Dreier <1568928+mdreier@users.noreply.github.com>
2022-03-18 17:11:12 +0100

    Übung 1.4

---
+++ R22_1.4_Statistik/R14_Statistik_3.R
@@ -0,0 +1,25 @@
+codeoceanR::rt_score()
+
+# A5 ----
+# Erzeuge 20 Zufallszahlen aus der Normalverteilung mit Mittelwert 350 und Standardabweichung 5.
+# (Ca. 95% der Zahlen sind dann zwischen 340 und 360; mu +- 2 sd).
+# t5_start
+zufallNorm <- 0
+# t5_ende
+
+
+# A6 ----
+# Erzeuge 600 Zufallszahlen aus der Beta Verteilung mit den Parametern 5 und 30.
+# (Das entspricht in binären Versuchen einem rechtsschiefen Anteil von 5/35.)
+# t6_start
+zufallBeta <- 0
+# t6_ende
+
+
+# A7 ----
+# Runde die normalverteilten Zufallszahlen auf 2 Nachkommastellen.
+zufallNormGerundet <- 0
+
+
+# Wenn du fertig bist, übermittle bitte deinen Punktestand an openHPI mit:
+# codeoceanR::rt_submit()
