768855faef84d0f52d028ae125ca548ced7a130e
Martin Dreier <1568928+mdreier@users.noreply.github.com>
2022-03-18 11:39:37 +0100

    Übung 1.3

---
+++ R22_1.3_Vektoren/R13_Vektoren_1.R
@@ -0,0 +1,37 @@
+codeoceanR::rt_score()
+
+# A1 ----
+# Füge folgende Zahlen zu einem Vektor zusammen: 8, -5.5, 27, 110
+vektorZahlen <- 0
+
+
+# Letzte Erinnerung: Löse sehr oft eine Bewertung aus (STRG + SHIFT + S).
+
+
+# A2 ----
+# Wiederhole die Zahl 5 77 mal.
+wiederholteZahl <- 0
+
+
+# A3 ----
+# Erstelle eine Sequenz von 500 bis 1 in Einserschritten.
+# CodeGolf Pro Tipp: Diese Aufgabe ist mit 5 Symbolen lösbar.
+zahlenSequenz1 <- 0
+
+
+# A4 ----
+# Erstelle eine Sequenz von 500 bis 0 in Abständen von 25.
+zahlenSequenz25 <- 0
+
+
+# A5 ----
+# Erstelle eine Sequenz von 500 bis 0 mit 30 Zahlen (in je gleichem Abstand).
+zahlenSequenz30 <- 0
+
+
+# A6 ----
+zahlenSequenz31 <- zahlenSequenz30
+# Füge der 'zahlenSequenz31' die Zahl -5 am Ende hinzu.
+
+
+# Mache weiter in "R13_Vektoren_2.R"
---
+++ R22_1.3_Vektoren/R13_Vektoren_2.R
@@ -0,0 +1,25 @@
+codeoceanR::rt_score()
+
+zahlenFolge <- seq(1, 20, len=15)
+
+# A7 ----
+# Erhalte das 12. Element von 'zahlenFolge'.
+element12 <- 0
+
+
+# A8 ----
+# Erhalte das 4. und 2. Element von 'zahlenFolge'.
+element4_2 <- 0
+
+
+# A9 ----
+# Erhalte alle Elemente von 'zahlenFolge', außer Elemente 8 bis 13.
+# Pro Tipp: negative Indizierung nutzen
+elementOhne <- 0
+
+
+# A10 ----
+# Ändere das 8. Element von 'zahlenFolge' zu 99.
+
+
+# Mache weiter in "R13_Vektoren_3.R"
