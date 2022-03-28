c7f01ee7c168998df933c0419516e1da22f1ed7f
Martin Dreier <1568928+mdreier@users.noreply.github.com>
2022-03-28 14:27:02 +0200

    Übung 2.2

---
+++ R22_2.2_Logik/R22_Logik_2.R
@@ -0,0 +1,34 @@
+codeoceanR::rt_score()
+
+# A4 ----
+# Schreibe eine Funktion mit den Argumenten A, B, C
+# Sie soll die R Variante von Wahr/Falsch herausgeben:
+# Wahr, wenn A und B wahr sind oder C wahr ist, sonst Falsch.
+logischeFunktion <- function(...) 0
+logischeFunktion(FALSE,TRUE,TRUE) # soll TRUE ausgeben
+
+
+# A5 ----
+# Eingebaute (built-in) Datensätze sind dokumentiert, siehe   ?rivers
+# Verwende `data` um Datensätze im globalEnv Panel anzuzeigen:
+data("rivers")
+
+# Wähle vom eingebauten Vektor 'rivers' alle Werte aus,
+# die kleiner/gleich 350 oder größer als 750 sind.
+# Die Auftretensreihenfolge ist beizubehalten.
+kleineGrosseFluesse <- 0
+
+
+# A6 ----
+# Welche Elemente im eingebauten Vektor 'state.area' sind kleiner als 50k?
+state.area
+state_index_klein <- 0
+
+
+# A7 ----
+# Welche der eingebauten Buchstaben `LETTERS` sind nicht gleich "K"?
+# Nutze den kurzmöglichsten Code (19 Zeichen).
+index_ungleich_K <- 0
+
+
+# Now continue in "ex_02_6_Logic_3.R"
